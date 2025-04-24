bbe42d5cc90a7ead847ebc6fa4cc642e5b0c87e8
Rin-Qi <yq2290@nyu.edu>
2025-04-24 17:12:05 -0400

    README changed and 404 added

--- web-app/app.py
+++ web-app/app.py
@@ -197,5 +197,10 @@ def avatar():
     return render_template("avatar.html")
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
