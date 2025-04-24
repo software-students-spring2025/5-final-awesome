813aff4deeb05c8676b7aa72d86241f16601d8da
Rin-Qi <yq2290@nyu.edu>
2025-04-24 17:19:42 -0400

    black

--- web-app/app.py
+++ web-app/app.py
@@ -199,7 +199,7 @@ def avatar():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
 
 if __name__ == "__main__":
